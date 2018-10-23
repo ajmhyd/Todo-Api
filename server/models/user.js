a81829e85103b75b9fd5298034b4f3f6290d2b82
Alex Matseshe <amatseshe@gmail.com>
2018-10-22 19:05:32 -0500

    Add POST /users and add token method

--- server/models/user.js
+++ server/models/user.js
@@ -1,12 +1,53 @@
 var mongoose = require('mongoose');
+var validator = require('validator');
+const jwt = require('jsonwebtoken');
+const _ = require('lodash');
 
 var userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
     trim: true,
-    minlength: 1
-  }
+    minlength: 1,
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: "{VALUE} is not a valid email"
+    }
+  },
+  password: {
+    type: String,
+    require: true,
+    minlength: 6
+  },
+  tokens: [{
+    access: {
+      type: String,
+      require: true
+    },
+    token: {
+      type: String,
+      require: true
+    }
+  }]
 });
+
+userSchema.methods.toJSON = function () {
+  var user = this;
+  var userObject = user.toObject();
+
+  return _.pick(userObject, ['_id', 'email']);
+};
+
+userSchema.methods.generateAuthToken = function () {
+  var user = this;
+  var access = 'auth';
+  var token = jwt.sign({_id: user._id.toHexString(), access}, 'abc123').toString();
+
+  user.tokens = user.tokens.concat([{access, token}]);
+  return user.save().then(() => {
+    return token;
+  });
+};
 
 module.exports = mongoose.model('User', userSchema);
--- server/server.js
+++ server/server.js
@@ -67,6 +67,7 @@ app.delete('/todos/:id', (req, res) => {
   });
 });
 
+//EDIT
 app.patch('/todos/:id', (req, res) => {
   var body = _.pick(req.body, ['text', 'completed']);
 
@@ -88,6 +89,19 @@ app.patch('/todos/:id', (req, res) => {
   }).catch((err) => {
     res.status(400).send();
   })
+});
+
+//CREATE USERS
+app.post('/users', (req,res) => {
+  var body = _.pick(req.body, ['email', 'password']);
+  var user = new User(body);
+  user.save().then(() => {
+    return user.generateAuthToken();
+  }).then((token) => {
+    res.header('x-auth', token).send(user);
+  }).catch((err) => {
+    res.status(400).send(err);
+  });
 });
 
 app.listen(port, () => {
